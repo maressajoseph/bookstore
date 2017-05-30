54048ef2d39ff80804374d8efedbac4252688854
vanGalilea <z.galili@icloud.com>
2017-05-30 10:38:12 +0200

    devise installed and configuerd

---
+++ db/migrate/20170530082855_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.string :author
+      t.text :description
+      t.decimal :price
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/products.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :product do
+    title "MyString"
+    author "MyString"
+    description "MyText"
+    price "9.99"
+    image "MyString"
+  end
+end
