67f7402f8332c159ee7ff1cdb7eabdd3454841b2
maressajoseph <maressa.joseph@gmail.com>
2017-05-30 15:17:21 +0200

    Add to cart works

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,12 +1,11 @@
 class CartsController < ApplicationController
 before_action :authenticate_user!
-before_action :set_product_id, except: [:index]
 
   def new
     set_product_id2
     @cart = Cart.new(product: @product, profile: current_user.profile)
     @cart.save
-    redirect_to product_cart_path(@product, @cart), notice: "Product added to your cart successuflly"
+    redirect_to carts_path, notice: "Product added to your cart successuflly"
   end
 
   def destroy
