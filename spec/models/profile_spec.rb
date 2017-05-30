59f8fa2ea6a83054950b6ebd3b500d83895fc2da
Claudia413 <cmjengelsman@gmail.com>
2017-05-30 16:51:49 +0200

    Added tests for profile model

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -4,4 +4,20 @@ RSpec.describe Profile, type: :model do
   describe "associations" do
       it { is_expected.to belong_to(:user) }
     end
+
+  describe "full-name display" do
+    let!(:profile) {create :profile, first_name: "Bob", last_name: "Sponge"}
+
+    it "returns full name of user" do
+      expect(profile.full_name).to eq "Bob Sponge"
+    end
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_presence_of(:last_name) }
+    it { is_expected.to validate_presence_of(:street) }
+    it { is_expected.to validate_presence_of(:zipcode) }
+    it { is_expected.to validate_presence_of(:city) }
+  end
 end
