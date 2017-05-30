c1bf7012dddaff3f40481104c0fa8c74ca62c81a
maressajoseph <maressa.joseph@gmail.com>
2017-05-30 14:54:30 +0200

    Updated home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,12 @@
 class PagesController < ApplicationController
 
   def index
+    @product1 = Product.first
+    @product2 = Product.second
+    @product3 = Product.third
   end
 
   def show
   end
-  
+
 end
