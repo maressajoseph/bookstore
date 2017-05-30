1c2b690b1db50d73d12ee651d808aa1ce2998598
vanGalilea <z.galili@icloud.com>
2017-05-30 10:57:43 +0200

    models, assoc and valid added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530083108) do
+ActiveRecord::Schema.define(version: 20170530085046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,18 @@ ActiveRecord::Schema.define(version: 20170530083108) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "street"
+    t.string "zipcode"
+    t.string "city"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +54,5 @@ ActiveRecord::Schema.define(version: 20170530083108) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
---
+++ spec/factories/profiles.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :profile do
+    first_name "MyString"
+    last_name "MyString"
+    street "MyString"
+    zipcode "MyString"
+    city "MyString"
+    user nil
+  end
+end
