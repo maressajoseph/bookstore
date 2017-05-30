e4a6cce217568ac01ab70e0b48305a7162d67cc0
vanGalilea <z.galili@icloud.com>
2017-05-30 14:44:37 +0200

    routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :products do
+    resources :carts, only: [:new]
+  end
+
+  resources :carts, only: [:index, :destroy]
+  root to: 'pages#index'
   resources :carts
-  resources :products
   resources :profiles, only: [:new, :edit, :create, :update]
 
 end
