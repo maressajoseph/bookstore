87b53ba86185e53cf6832fc9c73cd310c9bdd39a
ipresilia <ilsmarie.presilia@hotmail.com>
2017-05-30 11:42:05 +0200

    Created cart model, setup relationships

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,10 +1,10 @@
 class Profile < ApplicationRecord
   belongs_to :user
-
+  has_many :products, dependent: :destroy
   validates :first_name, :last_name, :street, :zipcode, :city, presence: true
 
   def full_name
     "#{first_name} #{last_name}"
   end
-  
+
 end
